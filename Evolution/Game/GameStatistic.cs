6146ce6647aa92c221472088a431f310e2a4ee9f
Andrii Repko <Andrey.Repko@3shape.com>
2020-04-25 20:22:23 +0300

    More statistic

---
+++ Evolution/Game/GameStatistic.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Evolution.Game
+{
+    public class GameStatistic
+    {
+        private readonly GameRunner _game;
+        private readonly Dictionary<int, double> _averageSpeed;
+
+        public IReadOnlyDictionary<int, double> AverageSpeedByDays => _averageSpeed;
+
+        public GameStatistic(GameRunner game)
+        {
+            _game = game;
+            _averageSpeed = new Dictionary<int, double>();
+        }
+
+        public int ZavrsCount
+        {
+            get
+            {
+                return _game.Zavrs.Count;
+            }
+        }
+
+        public int AverageAge
+        {
+            get
+            {
+                if (_game.Zavrs.Count == 0)
+                    return 0;
+                return (int)_game.Zavrs.Average(x => x.Age);
+            }
+        }
+
+        public double AverageSpeed
+        {
+            get
+            {
+                if (_game.Zavrs.Count == 0)
+                    return 0;
+                return 1.0 * _game.Zavrs.Sum(x => x.Speed) / _game.Zavrs.Count;
+            }
+        }
+
+        public double AverageSight
+        {
+            get
+            {
+                if (_game.Zavrs.Count == 0)
+                    return 0;
+
+                return 1.0 * _game.Zavrs.Sum(x => x.Sight) / _game.Zavrs.Count;
+            }
+        }
+
+        public int AverageEnergy
+        {
+            get
+            {
+                if (_game.Zavrs.Count == 0)
+                    return 0;
+                return (int)_game.Zavrs.Average(x => x.Energy);
+            }
+        }
+
+        public void Gather()
+        {
+            _averageSpeed[_game.Day] = AverageSpeed;
+        }
+    }
+}
---
+++ Evolution/Presenter/ChartControl/LineSeries.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Evolution.Presenter.ChartControl
+{
+    public class LineSeries:NotifierBase
+    {
+        private ObservableCollection<DataPoint> _data = new ObservableCollection<DataPoint>();
+        public ObservableCollection<DataPoint> Data
+        {
+            get { return _data; }
+            set
+            {
+                SetProperty(ref _data, value);
+            }
+        }
+
+        private string _name = "";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+    }
+}
